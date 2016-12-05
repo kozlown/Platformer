c2a5dd3b439d99d25747a5a24a3d730219edf835
NigelK <kozlown@gmail.com>
2016-12-05 13:46:25 +0100

    start button added

--- public/javascripts/eventHandler.js
+++ public/javascripts/eventHandler.js
@@ -51,7 +51,8 @@ game = null
             let gameId = value.id
             // title - id - join - start
             $( "#games" ).append( `<div class="game" id="${gameId}"><span class="name">${value.id + " - " + value.name}</span>`
-                    + `<a  class="btn btn-lg btn-outline" onclick="socket.emit('joinGame','${value.id}')">Join !</a></div>` )
+                    + `<a  class="btn btn-lg btn-outline" onclick="socket.emit('joinGame','${value.id}')">Join !</a>`
+                    + `<a  class="btn btn-lg btn-outline" onclick="socket.emit('startGame','${value.id}')">Start !</a></div>`)
 
             // players
             $( `#${gameId}` ).append("<div class='players'></div>")
