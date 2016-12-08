9ccdcc6f659e217a99d767abf3c2d59f5443f329
NigelK <kozlown@gmail.com>
2016-12-08 23:20:56 +0100

    front / server configs bug

--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,8 @@ router.get('/', function(req, res, next) {
           title: `Never forget who you are, the rest of the world will not. 
                   Wear it like armor and it will never be used to hurt you.`,
           welcome: `the World.`,
-          domain: configs.domain,
-          port: configs.port
+          domain: configsFront.domain,
+          port: configsFront.port
       }
   );
 });
