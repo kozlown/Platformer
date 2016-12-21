f7fbb7aa7f45eafb8a27a4ae8edae8a86a025c69
NigelK <kozlown@gmail.com>
2016-12-22 00:18:31 +0100

    handle innactive players
    players disconnect after a minute of innactivity

--- socket.io/socket.js
+++ socket.io/socket.js
@@ -11,7 +11,24 @@ module.exports = ( () => {
     return (socket) => {
 
         socket.on(`login`, (playerInfos) => {
+            // set the last action timestamp
+            socket.lastActionTimestamp = new Date().getTime()
+            // check every second if the last action was made in the last minute
+            // if not, disconnect the player
+            socket.checkActive = setInterval(()=> {
+                if (new Date().getTime() - socket.lastActionTimestamp > 60000){
+                    // if the player was playing and the game exists
+                    if (socket.player.gameId && gamesManager.getGame(socket.player.gameId)) {
+                        // he is disconnected from the game
+                        console.log("remove")
+                        gamesManager.getGame(socket.player.gameId).deleteElement(socket.player)
+                    }
 
+                    clearInterval(socket.checkActive)
+                    // tell the player he's disconnected
+                    socket.emit(`disconnected`);
+                }
+            }, 1000)
             // create a new player linked with the socket
             socket.player =
                 new Player(
@@ -31,165 +48,167 @@ module.exports = ( () => {
             // send current games to the player
             socket.emit(`currentGames`, gamesManager.getGamesInfos())
 
-        });
+            /**
+             * He's connected so he can play
+             */
+            socket.on(`logout`, () => {
 
-        socket.on(`logout`, () => {
+                // if the player was playing and the game exists
+                if (socket.player.game && gamesManager.getGame(socket.player.game.id)) {
+                    // he is disconnected from the game
+                    gamesManager.getGame(socket.player.game.id).removePlayer(socket.player)
+                }
 
-            // if the player was playing and the game exists
-            if (socket.player.game && gamesManager.getGame(socket.player.game.id)) {
-                // he is disconnected from the game
-                gamesManager.getGame(socket.player.game.id).removePlayer(socket.player)
-            }
+                // tell the player he's disconnected
+                socket.emit(`disconnected`);
 
-            // tell the player he's disconnected
-            socket.emit(`disconnected`);
+            });
 
-        });
+            socket.on(`keydown`, (keyCode) => {
+                // set the last action timestamp
+                socket.lastActionTimestamp = new Date().getTime()
+                switch (keyCode) {
 
-        socket.on(`keydown`, (keyCode) => {
+                    /**
+                     * Left
+                     */
+                    case 37:
+                        socket.player.state.isMovingLeft = true
+                        break
 
-            switch (keyCode) {
+                    /**
+                     * Right
+                     */
+                    case 39:
+                        socket.player.state.isMovingRight = true
+                        break
 
-                /**
-                 * Left
-                 */
-                case 37:
-                    socket.player.state.isMovingLeft = true
-                    break
+                    /**
+                     * Jump
+                     */
+                    case 38:
+                        socket.player.state.wantsToJump = true
+                        socket.player.jump()
+                        break
 
-                /**
-                 * Right
-                 */
-                case 39:
-                    socket.player.state.isMovingRight = true
-                    break
+                }
 
-                /**
-                 * Jump
-                 */
-                case 38:
-                    socket.player.state.wantsToJump = true
-                    socket.player.jump()
-                    break
+            })
 
-            }
+            socket.on(`keyup`, (keyCode) => {
+                // set the last action timestamp
+                socket.lastActionTimestamp = new Date().getTime()
+                switch (keyCode) {
 
-        })
+                    /**
+                     * Left
+                     */
+                    case 37:
+                        socket.player.state.isMovingLeft = false
+                        break
 
-        socket.on(`keyup`, (keyCode) => {
+                    /**
+                     * Right
+                     */
+                    case 39:
+                        socket.player.state.isMovingRight = false
+                        break
 
-            switch (keyCode) {
+                    /**
+                     * Jump
+                     */
+                    case 38:
+                        socket.player.state.wantsToJump = false
+                }
 
-                /**
-                 * Left
-                 */
-                case 37:
-                    socket.player.state.isMovingLeft = false
-                    break
+            })
 
-                /**
-                 * Right
-                 */
-                case 39:
-                    socket.player.state.isMovingRight = false
-                    break
+            socket.on(`newGame`, (gameInfos) => {
 
-                /**
-                 * Jump
-                 */
-                case 38:
-                    socket.player.state.wantsToJump = false
-            }
+                let game = new Game(gameInfos.name)
 
-        })
+                if (game.loadMap(gameInfos.map) && gamesManager.addGame(game)) { // if the game was successfully added
 
-        socket.on(`newGame`, (gameInfos) => {
+                    // send current games to everybody
+                    let gamesInfos = gamesManager.getGamesInfos()
 
-            let game = new Game(gameInfos.name)
+                    socket.emit('currentGames', gamesInfos)
+                    socket.broadcast.emit('currentGames', gamesInfos)
+                }
 
-            if (game.loadMap(gameInfos.map) && gamesManager.addGame(game)) { // if the game was successfully added
+            })
 
-                // send current games to everybody
+            socket.on(`joinGame`, (gameId) => {
+                // set the last action timestamp
+                socket.lastActionTimestamp = new Date().getTime()
+                let gameToJoin = gamesManager.getGame(gameId)
+
+                // if the game doesn't exist, exit
+                if (!gameToJoin) return
+
+                // Check if the player is already in a game
+                let isPlayerAlreadyInAGame = false
+                _.each(gamesManager.games, (game, index, array) => {
+                    _.each(game.getElementsOfType(Player), (player, index, array) => {
+                        if (player.id === socket.player.id) {
+                            isPlayerAlreadyInAGame = game.id
+                        }
+                    })
+                })
+                // set the socket gameID
+                socket.gameId = gameId
+
+                // if he's in a game then make him exit this game
+                if (isPlayerAlreadyInAGame) {
+                    gamesManager.getGame(isPlayerAlreadyInAGame).deleteElement(socket.player)
+                }
+
+                // determine if the player should be a Zombie or a Runner
+                // if there are more Runners than Zombies
+                if (gameToJoin.getElementsOfType(Runner).length > gameToJoin.getElementsOfType(Zombie).length){
+                    // the new Player is a Zombie
+                    socket.player = new Zombie(socket.player)
+                }
+                // else
+                else {
+                    // the new Player is a Runner
+                    socket.player = new Runner(socket.player)
+                }
+
+                // set his position to a random respawn point of the game
+                let respawns = gameToJoin.getElementsOfType(Respawn)
+                let respawn = respawns[_.random(0, respawns.length - 1)]
+                socket.player.setPosition(respawn.position.x, respawn.position.y)
+
+                // make him join the game
+                gameToJoin.addElement(socket.player)
+
+                // he get the infos so he can display the map
+                socket.emit("newGame", gameToJoin.getGameUpdateInfos(socket.player))
+
+                // send new current games infos to the players
                 let gamesInfos = gamesManager.getGamesInfos()
 
                 socket.emit('currentGames', gamesInfos)
                 socket.broadcast.emit('currentGames', gamesInfos)
-            }
 
-        })
-
-        socket.on(`joinGame`, (gameId) => {
-            let gameToJoin = gamesManager.getGame(gameId)
-
-            // if the game doesn't exist, exit
-            if (!gameToJoin) return
-
-            // Check if the player is already in a game
-            let isPlayerAlreadyInAGame = false
-            _.each(gamesManager.games, (game, index, array) => {
-                _.each(game.getElementsOfType(Player), (player, index, array) => {
-                    if (player.id === socket.player.id) {
-                        isPlayerAlreadyInAGame = game.id
-                    }
-                })
             })
-            // set the socket gameID
-            socket.gameId = gameId
 
-            // if he's in a game then make him exit this game
-            if (isPlayerAlreadyInAGame) {
-                gamesManager.getGame(isPlayerAlreadyInAGame).deleteElement(socket.player)
-            }
+            socket.on(`startGame`, (gameId) => {
+                // set the last action timestamp
+                socket.lastActionTimestamp = new Date().getTime()
 
-            // determine if the player should be a Zombie or a Runner
-            // if there are more Runners than Zombies
-            if (gameToJoin.getElementsOfType(Runner).length > gameToJoin.getElementsOfType(Zombie).length){
-                // the new Player is a Zombie
-                socket.player = new Zombie(socket.player)
-            }
-            // else
-            else {
-                // the new Player is a Runner
-                socket.player = new Runner(socket.player)
-            }
+                let gameToStart = gamesManager.getGame(gameId)
 
-            // set his position to a random respawn point of the game
-            let respawns = gameToJoin.getElementsOfType(Respawn)
-            let respawn = respawns[_.random(0, respawns.length - 1)]
-            socket.player.setPosition(respawn.position.x, respawn.position.y)
+                // if the game doesn't exist, exit
+                if (!gameToStart) return
 
-            // make him join the game
-            gameToJoin.addElement(socket.player)
+                // else, start the game
+                gameToStart.start()
 
-            // he get the infos so he can display the map
-            socket.emit("newGame", gameToJoin.getGameUpdateInfos(socket.player))
+            })
 
-            // send new current games infos to the players
-            let gamesInfos = gamesManager.getGamesInfos()
-
-            socket.emit('currentGames', gamesInfos)
-            socket.broadcast.emit('currentGames', gamesInfos)
-
-        })
-
-        socket.on(`becomeZombie`, () => {
-
-            let gameId = socket.player.gameId
-            gamesManager.getGame(gameId).deleteElement(socket.player)
-            gamesManager.getGame(gameId).addElement(new Zombie(socket.player))
-
-        })
-
-        socket.on(`startGame`, (gameId) => {
-            let gameToStart = gamesManager.getGame(gameId)
-
-            // if the game doesn't exist, exit
-            if (!gameToStart) return
-
-            // else, start the game
-            gameToStart.start()
-
-        })
+        });
     }
 
 } );
